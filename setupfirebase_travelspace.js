d5a5c844c3c72e9d9338527d1a32af5210b8e8dd
schang154 <shchang1128@gmail.com>
2019-03-30 12:43:00 -0700

    after user logged in, the user will be added to the database

---
+++ setupfirebase_travelspace.js
@@ -0,0 +1,16 @@
+(function(){
+
+  // Initialize Firebase
+  var config = {
+
+      apiKey: "AIzaSyBRhEm0WFOVmSqcRx80650G7KhE1q58o5A",
+      authDomain: "travel-space-9f0a2.firebaseapp.com",
+      databaseURL: "https://travel-space-9f0a2.firebaseio.com",
+      projectId: "travel-space-9f0a2",
+      storageBucket: "travel-space-9f0a2.appspot.com",
+      messagingSenderId: "738064646445"
+  };
+  firebase.initializeApp(config);
+  app_firebase = firebase;
+})();
+
